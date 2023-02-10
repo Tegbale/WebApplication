b1930dd87e0ffde046c3bf5857ac99dc598d255a
karosaxy <araromirichard@gmail.com>
2023-02-10 15:35:16 +0100

    added a partial link button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,13 @@
 
 <template>
   <main>
-    <h1>Home page</h1>
+    <div class="w-sreen h-screen flex space-x-4 justify-center items-center">
+      <h1>Home page</h1>
+      <router-link to="/login">
+        <p class="bg-red-500 w-fit text-white font-bold py-2 px-4 rounded">
+          Go To Login
+        </p>
+      </router-link>
+    </div>
   </main>
 </template>
